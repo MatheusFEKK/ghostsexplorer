4e610c1f06c8e8f4aa2559e506d677863faf872b
Matheus Felipe <matheusfelipelazarini4@gmail.com>
2024-11-28 01:03:33 -0300

    Funcionando o cadastro, com timer e score do usuário

--- script.js
+++ script.js
@@ -1,52 +1,5 @@
-document.getElementById("botaoCadastrar").addEventListener("click", function () {
-    let nome = document.getElementById("nome").value.trim();
-    let idade = parseInt(document.getElementById("idade").value.trim());
-    let poder = document.getElementById("poder").value.trim();
-    let mensagem = document.getElementById("mensagem");
-    let increasePoints = 0
-    let holder = 0
-    
-    if (!nome || !idade || !poder)  {
-        mensagem.innerText = "Erro: Todos os campos devem ser preenchidos corretamente!";
-        mensagem.style.color = "red";
-        return;
-    }
-    
-    document.getElementById("botaoCadastrar").addEventListener("click", function(){
-        let nome = document.getElementById("nome").value.trim();
-        let idade = parseInt(document.getElementById("idade").value.trim());
-        let poder = document.getElementById("poder").value.trim();
-        if (nome === "" || idade <= 0 || poder === ""){
-            mensagem.innerText = "Erro: Todos os campos devem ser preenchidos corretamente!";
-            mensagem.style.color = "red";
-            return;
-        } 
-        
-    });
-    
-    document.getElementById("botaoCadastrar").addEventListener("click", function (){
-        increasePoints++
-        console.log(increasePoints);
-    });
-    let tabela = document.getElementById("tabelaFantasma").querySelector("tbody");
-    let novaLinha = tabela.insertRow();
-    novaLinha.insertCell(0).innerText = nome;
-    novaLinha.insertCell(1).innerText = idade;
-    novaLinha.insertCell(2).innerText = poder;
-    
-    mensagem.innerText = `Fantasma ${nome} cadastrado com sucesso!`;
-    mensagem.style.color = "green";
-    
-    document.getElementById("nome").value = "";
-    document.getElementById("idade").value = "";
-    document.getElementById("poder").value = "";
-    
-
-    
-});
-
 let countDown = 30;
-
+let points = document.getElementById("points");
 let timer = setInterval(() => {
     countDown--
     let spanTimer = document.getElementById("timer");
@@ -54,6 +7,8 @@ let timer = setInterval(() => {
     console.log(countDown)
     if (countDown <= 0){
         clearInterval(timer)
+        let pointsByUser = points.value
+        window.alert('O tempo acabou, você conseguiu efetuar ' + pointsByUser + ' cadastros de fantasmas')
         tabela.innerHTML = "";  
     }
     
@@ -65,3 +20,45 @@ let timer = setInterval(() => {
     }, 1000)
     
 let tabela = document.getElementById("tabelaFantasma").querySelector("tbody");
+
+
+let holder = 0
+document.getElementById("botaoCadastrar").addEventListener("click", function () {
+    let nome = document.getElementById("nome").value.trim();
+    let idade = parseInt(document.getElementById("idade").value.trim());
+    let poder = document.getElementById("poder").value.trim();
+    let mensagem = document.getElementById("mensagem");
+    let points = document.getElementById("points");
+    let increasePoints = 0
+
+
+    if (!nome || !idade || !poder)  {
+        mensagem.innerText = "Erro: Todos os campos devem ser preenchidos corretamente!";
+        mensagem.style.color = "red";
+        return;
+    }
+
+    increasePoints++;
+    holder = holder + increasePoints;
+    points.value =  holder;
+
+        // Caso queira determianar quantos cadastro o usuario tem que fazer (definir uma quantidade)
+    // if (points.value == 5){
+    //     window.alert('Parabens você conseguiu cadastrar 5 fantasmas');
+    // }
+
+    let tabela = document.getElementById("tabelaFantasma").querySelector("tbody");
+    let novaLinha = tabela.insertRow();
+    novaLinha.insertCell(0).innerText = nome;
+    novaLinha.insertCell(1).innerText = idade;
+    novaLinha.insertCell(2).innerText = poder;
+
+
+    mensagem.innerText = `Fantasma ${nome} cadastrado com sucesso!`;
+    mensagem.style.color = "green";
+
+    document.getElementById("nome").value = "";
+    document.getElementById("idade").value = "";
+    document.getElementById("poder").value = "";
+
+});
